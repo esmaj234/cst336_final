ee18ac127921f79844a36f63adcc62fabd9e45ef
sentinel49 <126994179+sentinel49@users.noreply.github.com>
2023-12-11 00:24:50 +0000

    Created dbPool.js to connect to database.  Added dbTest route (for testing db connection) to index.js.  Added executeSQL function to index.js to allow retrieval from database.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const pool = require("./dbPool.js"); // LC: link to database
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
@@ -9,3 +10,20 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
   console.log("server started");
 });
+
+// LC: Database Test (see dbPool.js for other table names)
+app.get("/dbTest", async function(req, res) {
+  let sql = "SELECT * FROM product";
+  let rows = await executeSQL(sql);
+  res.send(rows);
+});
+
+// LC: Function to execute SQL
+async function executeSQL(sql, params) {
+  return new Promise (function(resolve, reject) {
+    pool.query(sql, params, function (err, rows, fields) {
+      if (err) throw err;
+      resolve(rows);
+    });
+  });
+}
