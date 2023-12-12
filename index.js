e1abc3608ed684f5c4b28c1b14d0ed1cca9a4351
esmaj234 <126372717+esmaj234@users.noreply.github.com>
2023-12-12 04:10:56 +0000

    Add more placeholder code + one working API

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require("express");
 const app = express();
+const fetch = require("node-fetch");
 const pool = require("./dbPool.js"); // LC: link to database
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/checkout", async (req, res) => {
+  let sql = "SELECT * FROM cart";
+  let rows = await executeSQL(sql);
+  res.render("checkout", { items: rows });
+});
+
+app.get("/confirm", (req, res) => {
+  res.render("ship", { city: "", state: "" });
+});
+
+// Function to get state and city from zip
+app.get("/getShipInfo", async (req, res) => {
+  let zip = req.query.zip;
+  let url = `https://csumb.space/api/cityInfoAPI.php?zip=${zip}`;
+  let response = await fetch(url);
+  let data = await response.json();
+  console.log(data.city);
+  console.log(data.state);
+  res.render("ship", { city: data.city, state: data.state });
 });
 
 app.listen(3000, () => {
@@ -12,7 +35,7 @@ app.listen(3000, () => {
 });
 
 // LC: Database Test (see dbPool.js for other table names)
-app.get("/dbTest", async function(req, res) {
+app.get("/dbTest", async function (req, res) {
   let sql = "SELECT * FROM product";
   let rows = await executeSQL(sql);
   res.send(rows);
@@ -20,7 +43,7 @@ app.get("/dbTest", async function(req, res) {
 
 // LC: Function to execute SQL
 async function executeSQL(sql, params) {
-  return new Promise (function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
     pool.query(sql, params, function (err, rows, fields) {
       if (err) throw err;
       resolve(rows);
